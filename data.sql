1f4e031d60a81ded12aacaf6fffb1af7e5a39bde
diana <diliang4783573@gmail.com>
2021-02-15 17:40:09 -0800

    Initial Golang setup

---
+++ data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  userid SERIAL PRIMARY KEY,
+  username TEXT,
+  ign TEXT, 
+  world TEXT
+)
+
---
+++ models/user.go
@@ -0,0 +1,10 @@
+package user
+
+// represents the `users` table
+// uppercase means it is public or exported and can be accessed by other packages.
+type User struct {
+	ID int64 `json:"id"`
+  Username string `json:"username"`
+  Ign string `json:"ign"` 
+  World string `json:"world"`
+}
